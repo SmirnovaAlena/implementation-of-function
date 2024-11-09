e79cd5015821e2cff1a5fc5c9566372c5b3580c4
SmirnovaAlena <smirnova.alenad@gmail.com>
2024-11-09 17:57:36 +0300

    Add files via upload

---
+++ mystrlen.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+using namespace std;
+//  функция определяет длину строки.
+
+int mystrlen(const char* str) {
+int len = 0;
+while (*str != '\0') {
+	len += 1;
+	str += 1;
+}
+return len;
+}
+
+int main() {
+	char str[256];
+	cin >> str;
+	cout << "size string: " << mystrlen(str);
+
+}

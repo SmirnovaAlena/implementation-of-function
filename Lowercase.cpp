b081d8105853a856f6870aa91b1465a5cbe8174d
SmirnovaAlena <smirnova.alenad@gmail.com>
2024-11-09 17:52:30 +0300

    Add files via upload

---
+++ Lowercase.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+using namespace std;
+//      перевод в нижний регистр    
+
+char* Lowercase(char* str) {
+	while (*str != '\0') {
+		*str = *str + 32;
+		cout << * str;
+		str++;
+
+	}
+	return str;
+}
+int main() {
+	char* str1 = new char[256];
+	cout << "enter string: " << endl;
+	cin >> str1;
+	cout << Lowercase(str1);
+}

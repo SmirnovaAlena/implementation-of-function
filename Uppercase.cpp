88c375e6dcb6361b29c1d8cd3f38e83381db858e
SmirnovaAlena <smirnova.alenad@gmail.com>
2024-11-09 17:50:30 +0300

    Add files via upload

---
+++ Uppercase.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+using namespace std;
+//    перевод в верхний регистр
+
+char* Uppercase(char* str) {
+	while (*str != '\0') {
+		*str = *str - 32;
+		cout << *str;
+		str++;
+
+	}
+	return str;
+}
+int main() {
+	char* str1 = new char[128];
+	cout << "enter string: " << endl;
+	cin >> str1;
+	Uppercase(str1);
+}

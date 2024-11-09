7f827b865170dd203cbf080752b1b06060ccc4a8
SmirnovaAlena <smirnova.alenad@gmail.com>
2024-11-09 18:04:43 +0300

    Add files via upload

---
+++ mystrchr.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+using namespace std;
+//  функция осуществляет поиск символа s в строке str
+
+char* mystrchr(char* str, char s) {
+	while (*str != '\0') {
+
+		if (*str == s)
+		{
+			return str;
+		} str++;
+	}
+	return 0;
+}
+
+int main() {
+	char* str = new char[128];
+	char s;
+	cin >> str >> s;
+	cout << mystrchr(str, s);
+}

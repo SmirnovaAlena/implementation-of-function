ac805891227ab492114f5cc7b8c0431a74a994fb
SmirnovaAlena <smirnova.alenad@gmail.com>
2024-11-09 18:06:56 +0300

    Add files via upload

---
+++ mystrstr.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+using namespace std;
+char* mystrstr(char* str1, char* str2) {
+    while (*str1 != '\0')
+    {
+        if (*str1 == *str2)
+        {
+            return str1;
+        }
+        str1++;
+    }
+    return str1;
+}
+
+int main() {
+	char* str1 = new char[128];
+	char* str2 = new char[128];
+	cin >> str1;
+	cin >> str2;
+	cout << mystrstr(str1, str2);
+}
